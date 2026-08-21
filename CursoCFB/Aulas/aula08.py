261591916907c35ed0ca664ee37b02a525e7357b
CaioStack <caiosalmarques7@gmail.com>
2026-08-21 20:37:18 -0300

    adicionar conteúdo sobre tipos booleanos em aula08.py

--- CursoCFB/Aulas/aula08.py
+++ CursoCFB/Aulas/aula08.py
@@ -1,2 +1,25 @@
 # Aula 08 - Tipo Boolean
 
+aula = True
+print(aula) # Imprime o valor booleano True
+
+aula2 = False
+print(aula2) # Imprime o valor booleano False
+
+calc = 10 > 5 # Verifica se 10 é maior que 5, o resultado é True
+print(calc) # Imprime o valor booleano True
+
+calc2 = 10 < 5 # Verifica se 10 é menor que 5, o resultado é False
+print(calc2) # Imprime o valor booleano False
+
+aula3 = "CFB Cursos"
+print((bool(aula3))) # Imprime o valor booleano True, pois a string não está vazia
+
+vazio = ""
+print((bool(vazio))) # Imprime o valor booleano False, pois a string está vazia
+
+num = 0
+print((bool(num))) # Imprime o valor booleano False, pois o número é zero
+
+num2 = 10
+print((bool(num2))) # Imprime o valor booleano True, pois o número é diferente de zero
