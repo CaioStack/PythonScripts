c8725fa89fc016d8b91c3ffe1672a4007438fdf7
CaioStack <caiosalmarques7@gmail.com>
2026-08-22 20:52:17 -0300

    adicionar funções de soma, textos e carros ao arquivo aula20.py

--- CursoCFB/Aulas/aula20.py
+++ CursoCFB/Aulas/aula20.py
@@ -1,1 +1,30 @@
 # Aula 20 - Funções P2
+
+# valores = [1, 5, 3, 2]
+
+def somar(*num):
+
+    r = 0
+
+    for n in num:
+        r += n
+    print("Soma: "+ str(r))
+
+somar(5, 7)
+somar(12, 8, 3)
+somar(1, 2, 6, 9)
+
+def textos(*txt):
+
+    for t in txt:
+
+        print(t)
+
+textos("CFB Cursos", "Python", "Canal", "Curso", "Computador")
+
+def carros (c):
+    print("Modelo: " + c)
+
+carros("HRV")
+
+# somar(valores) # Lista valores(lá em cima) dentro da função somar
