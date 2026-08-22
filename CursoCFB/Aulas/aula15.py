22472d5edb757314754980462809b28472a85f63
CaioStack <caiosalmarques7@gmail.com>
2026-08-22 17:33:30 -0300

    adicionar comentários explicativos sobre tuplas e listas no arquivo aula15.py

--- CursoCFB/Aulas/aula15.py
+++ CursoCFB/Aulas/aula15.py
@@ -1,1 +1,16 @@
 # Aula 15 - Tuplas
+
+# As listas são mutáveis, as tuplas são imutáveis
+
+# l_carros = ["HRV", "Golf", "Argo"]
+t_carros = ("HRV", "Golf", "Argo")
+
+# Uma forma de "deixar" tuplas mutáveis
+
+t2_carros = list(t_carros) # Convertendo de tupla para lista, assim ficando mutável
+
+t2_carros[2] = "Focus" # Trocando o item 2 "Argo" para "Focus" em formato de lista
+t_carros = tuple(t2_carros) # Trocando de volta, de lista para tupla
+
+for x in t_carros:
+    print(x)
