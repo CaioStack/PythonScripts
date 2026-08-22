a9e4f1928753c08d61231ce4eafa2407cfccdee5
CaioStack <caiosalmarques7@gmail.com>
2026-08-22 18:53:08 -0300

    adicionar implementação da lista de carros com detalhes e impressão formatada

--- CursoCFB/Aulas/aula16.py
+++ CursoCFB/Aulas/aula16.py
@@ -1,1 +1,15 @@
 # Aula 16 - Matrizes
+
+carros = [
+    ["Modelo", "HRV"], 
+    ["Fabricante", "Honda"], 
+    ["Ano", 2016]
+    ]
+
+# print(carros[2][0]) -> # Ano
+
+carros.append(["Cor", "Prata"])
+carros[2][1] = 2019
+
+for l, c in carros:
+    print(l + " | " + str(c))
