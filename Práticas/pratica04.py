be32584efcb58a29ace8cafc0f5b91b3a5ed9518
CaioStack <caiosalmarques7@gmail.com>
2026-08-23 18:37:52 -0300

    adicionar novo arquivo pratica04.py com questões sobre strings

---
+++ Práticas/pratica04.py
@@ -0,0 +1,13 @@
+# Conteúdos abordados nesse arquivo de pratica04.py: Strings
+
+# Questão 1:
+# Crie uma variável com seu nome completo e exiba o primeiro e o último caractere dessa string, utilizando índices.
+# TODO: fazer a questão 1
+
+# Questão 2:
+# Dada a string frase = "Programação Estruturada", exiba apenas os 5 primeiros caracteres utilizando fatiamento (slicing).
+# TODO: fazer a questão 2
+
+# Questão 3:
+# Concatene duas variáveis (um nome e um sobrenome) utilizando o operador +, exibindo o nome completo separado por um espaço.
+# TODO: fazer a questão 3
