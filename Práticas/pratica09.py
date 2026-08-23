95590bbe3ab699b5c70e48ef00db1bd1ef77f407
CaioStack <caiosalmarques7@gmail.com>
2026-08-23 18:38:10 -0300

    adicionar novo arquivo pratica09.py com questões sobre métodos avançados de coleções list

---
+++ Práticas/pratica09.py
@@ -0,0 +1,17 @@
+# Conteúdos abordados nesse arquivo de pratica09.py: Coleção list (métodos avançados)
+
+# Questão 1:
+# Dada uma lista de números desordenados, ordene-a utilizando o método sort() e exiba a lista ordenada.
+# TODO: fazer a questão 1
+
+# Questão 2:
+# Dada uma lista de números, remova um elemento específico utilizando o método remove() e exiba a lista resultante.
+# TODO: fazer a questão 2
+
+# Questão 3:
+# Dada uma lista com números repetidos, utilize o método count() para contar quantas vezes um valor específico aparece nela.
+# TODO: fazer a questão 3
+
+# Questão 4:
+# Concatene duas listas diferentes utilizando o operador + e exiba a lista resultante.
+# TODO: fazer a questão 4
