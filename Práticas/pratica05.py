160f391085edaf3ecb04d703f8feef2880635409
CaioStack <caiosalmarques7@gmail.com>
2026-08-23 18:37:56 -0300

    adicionar novo arquivo pratica05.py com questões sobre métodos de strings

---
+++ Práticas/pratica05.py
@@ -0,0 +1,17 @@
+# Conteúdos abordados nesse arquivo de pratica05.py: Strings (métodos)
+
+# Questão 1:
+# Dada a string texto = "   Python é incrível   ", remova os espaços em branco das extremidades utilizando o método strip() e exiba o resultado.
+# TODO: fazer a questão 1
+
+# Questão 2:
+# Utilizando os métodos upper() e lower(), exiba uma mesma frase primeiro toda em maiúsculas e depois toda em minúsculas.
+# TODO: fazer a questão 2
+
+# Questão 3:
+# Dada a string frase = "a linguagem python é muito utilizada", utilize o método count() para contar quantas vezes a letra "a" aparece.
+# TODO: fazer a questão 3
+
+# Questão 4:
+# Utilizando o método split(), separe a frase "Estruturas de repetição são fundamentais" em uma lista de palavras e exiba essa lista.
+# TODO: fazer a questão 4
