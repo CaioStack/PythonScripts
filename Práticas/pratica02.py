fbe2a77229e1def18202c5b3968b4a5e35663624
CaioStack <caiosalmarques7@gmail.com>
2026-08-23 18:37:36 -0300

    adicionar novo arquivo pratica02.py com questões sobre tipos de dados

---
+++ Práticas/pratica02.py
@@ -0,0 +1,13 @@
+# Conteúdos abordados nesse arquivo de pratica02.py: Tipos de dados
+
+# Questão 1:
+# Crie uma variável de cada tipo básico do Python (int, float, str, bool) e utilize a função type() para exibir o tipo de cada uma.
+# TODO: fazer a questão 1
+
+# Questão 2:
+# Dada a variável valor = 42, utilize a função isinstance() para verificar se ela é do tipo int, exibindo o resultado.
+# TODO: fazer a questão 2
+
+# Questão 3:
+# Crie uma lista contendo valores de tipos diferentes (int, str, float, bool) e, utilizando um laço for, exiba o tipo de cada elemento da lista.
+# TODO: fazer a questão 3
