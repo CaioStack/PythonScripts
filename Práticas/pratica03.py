94e204124e85d09cd7adb2f0be93cb6c4d58842f
CaioStack <caiosalmarques7@gmail.com>
2026-08-23 18:37:49 -0300

    adicionar novo arquivo pratica03.py com questões sobre operações de casting

---
+++ Práticas/pratica03.py
@@ -0,0 +1,13 @@
+# Conteúdos abordados nesse arquivo de pratica03.py: Operações de casting
+
+# Questão 1:
+# Crie duas variáveis com números representados como string (ex.: "10" e "3"), converta-as para int e exiba a soma entre elas.
+# TODO: fazer a questão 1
+
+# Questão 2:
+# Dada a variável valor = "3.75", converta-a primeiro para float e depois para int, exibindo os dois resultados obtidos.
+# TODO: fazer a questão 2
+
+# Questão 3:
+# Crie uma variável do tipo booleano (True ou False) e converta-a para int, exibindo o resultado dessa conversão.
+# TODO: fazer a questão 3
