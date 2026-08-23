ca858a3537cc13dd7f62e22fb996d22d848c2826
CaioStack <caiosalmarques7@gmail.com>
2026-08-23 18:38:03 -0300

    adicionar novo arquivo pratica07.py com questões sobre casting, boolean e input

---
+++ Práticas/pratica07.py
@@ -0,0 +1,13 @@
+# Conteúdos abordados nesse arquivo de pratica07.py: Casting + Boolean + Input
+
+# Questão 1:
+# Simule a leitura de dois números digitados pelo usuário (utilizando input()), converta-os para int e exiba a soma, a subtração e a multiplicação entre eles.
+# TODO: fazer a questão 1
+
+# Questão 2:
+# Peça ao usuário sua idade utilizando input(), converta o valor para int e exiba o resultado (True ou False) da comparação que verifica se a pessoa é maior de idade.
+# TODO: fazer a questão 2
+
+# Questão 3:
+# Peça ao usuário dois valores numéricos utilizando input(), converta-os para float e calcule a média entre eles.
+# TODO: fazer a questão 3
