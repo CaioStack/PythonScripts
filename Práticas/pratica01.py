f1a2fab12f2d8f95333c9b3a38b8102325a1b107
CaioStack <caiosalmarques7@gmail.com>
2026-08-23 18:37:33 -0300

    adicionar novo arquivo pratica01.py com questões sobre variáveis

---
+++ Práticas/pratica01.py
@@ -0,0 +1,13 @@
+# Conteúdos abordados nesse arquivo de pratica01.py: Variáveis
+
+# Questão 1:
+# Crie três variáveis para armazenar seu nome, sua idade e sua altura. Em seguida, exiba os três valores utilizando print().
+# TODO: fazer a questão 1
+
+# Questão 2:
+# Crie duas variáveis numéricas, a e b, e troque os valores entre elas (sem criar uma terceira variável). Exiba os valores antes e depois da troca.
+# TODO: fazer a questão 2
+
+# Questão 3:
+# Crie uma variável x com o valor 10. Sem apagar a variável, faça x receber x + 5 e exiba o novo valor.
+# TODO: fazer a questão 3
