d5a64a5a559b7de5679eefa7c9c1808b34f60d70
CaioStack <caiosalmarques7@gmail.com>
2026-08-23 18:38:06 -0300

    adicionar novo arquivo pratica08.py com questões sobre coleções list

---
+++ Práticas/pratica08.py
@@ -0,0 +1,13 @@
+# Conteúdos abordados nesse arquivo de pratica08.py: Coleção list
+
+# Questão 1:
+# Crie uma lista com 5 números inteiros e exiba o primeiro, o último e o elemento do meio, utilizando índices.
+# TODO: fazer a questão 1
+
+# Questão 2:
+# Adicione um novo elemento ao final de uma lista já existente, utilizando o método append(), e exiba a lista antes e depois.
+# TODO: fazer a questão 2
+
+# Questão 3:
+# Dada uma lista de números, exiba a quantidade de elementos dessa lista utilizando a função len().
+# TODO: fazer a questão 3
