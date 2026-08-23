218ced405677f003e32c6a5836219cc23ada18fd
CaioStack <caiosalmarques7@gmail.com>
2026-08-23 18:37:59 -0300

    adicionar novo arquivo pratica06.py com questões sobre tipo boolean

---
+++ Práticas/pratica06.py
@@ -0,0 +1,13 @@
+# Conteúdos abordados nesse arquivo de pratica06.py: Tipo boolean
+
+# Questão 1:
+# Crie duas variáveis numéricas e utilize operadores de comparação (>, <, ==) para gerar e exibir três resultados booleanos diferentes.
+# TODO: fazer a questão 1
+
+# Questão 2:
+# Combine duas condições utilizando os operadores and e or, exibindo o resultado de cada combinação.
+# TODO: fazer a questão 2
+
+# Questão 3:
+# Utilizando o operador not, inverta o valor de uma expressão booleana e exiba o resultado antes e depois da inversão.
+# TODO: fazer a questão 3
