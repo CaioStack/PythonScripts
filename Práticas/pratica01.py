94bec1f91ba453d133ac6b9733b45fd12b12456b
CaioStack <caiosalmarques7@gmail.com>
2026-08-27 21:12:31 -0300

    adicionar implementação das questões 1, 2 e 3 no arquivo pratica01.py

--- Práticas/pratica01.py
+++ Práticas/pratica01.py
@@ -2,12 +2,25 @@
 
 # Questão 1:
 # Crie três variáveis para armazenar seu nome, sua idade e sua altura. Em seguida, exiba os três valores utilizando print().
-# TODO: fazer a questão 1
+
+nome = "Caio Salgado Marques"
+idade = 17
+altura = 1.69
+
+print("Nome: " + nome, "Idade: " + str(idade), "Altura: " + str(altura))
 
 # Questão 2:
 # Crie duas variáveis numéricas, a e b, e troque os valores entre elas (sem criar uma terceira variável). Exiba os valores antes e depois da troca.
-# TODO: fazer a questão 2
+a, b = 10, 20
+print(a,b)
+
+b, a = b, a
+print(b, a)
 
 # Questão 3:
 # Crie uma variável x com o valor 10. Sem apagar a variável, faça x receber x + 5 e exiba o novo valor.
-# TODO: fazer a questão 3
+
+x = 10
+x += 5
+
+print(x)
