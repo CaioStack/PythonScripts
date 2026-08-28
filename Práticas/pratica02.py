d99cab2001a55088ba81dde34111d92a23f86900
CaioStack <caiosalmarques7@gmail.com>
2026-08-27 21:12:37 -0300

    adicionar implementação das questões 1 e 2 no arquivo pratica02.py

--- Práticas/pratica02.py
+++ Práticas/pratica02.py
@@ -2,11 +2,27 @@
 
 # Questão 1:
 # Crie uma variável de cada tipo básico do Python (int, float, str, bool) e utilize a função type() para exibir o tipo de cada uma.
-# TODO: fazer a questão 1
+
+int = 10
+float = 0.39
+str = "String"
+bool = True
+
+typeint = type(int)
+typefloat = type(float)
+typestr = type(str)
+typebool = type(bool)
+
+print(typeint)
+print(typefloat)
+print(typestr)
+print(typebool)
 
 # Questão 2:
 # Dada a variável valor = 42, utilize a função isinstance() para verificar se ela é do tipo int, exibindo o resultado.
-# TODO: fazer a questão 2
+
+valor = 42
+print(isinstance(valor, int))
 
 # Questão 3:
 # Crie uma lista contendo valores de tipos diferentes (int, str, float, bool) e, utilizando um laço for, exiba o tipo de cada elemento da lista.
